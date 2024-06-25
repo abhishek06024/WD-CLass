09600aea62e923f00a041a2390b190b7dd5eb776
Abhishek Sharma <skye060624@gmail.com>
2024-06-25 17:15:37 +0530

    Another commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict"
+// var
+// let 
+// const
+console.log("Hello Developers");
+// Variables 
+
+var a = 10 
+let b = 20
+const c=30
+console.log(a+b)
+console.log(b)
+// templet string 
+
+console.log(a,b);
+// the value of a is a and the value of b is b
+console.log("the value of a is"+a+"and the value of b is"+b);
+var a =50 
+ b= 70; // This is an error
+console.log(`the value of a is ${a} and the value of b is ${b}`);
+// c = 95;
