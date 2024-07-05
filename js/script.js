d8bbbeb9810972b0d9a1ed30dd1191bf47fe5594
Abhishek Sharma <skye060624@gmail.com>
2024-07-05 16:06:23 +0530

    js code

--- js/script.js
+++ js/script.js
@@ -2,20 +2,34 @@
 // var
 // let 
 // const
-console.log("Hello Developers");
+console.log("Hello Kunal");
 // Variables 
 
-var a = 10 
+var a = "kunal" 
 let b = 20
-const c=30
-console.log(a+b)
+const c = 30
+
+var a ='priyanshu'
+// let b='Aksha'
+
+console.log(a)
 console.log(b)
+console.log(c)
+
+let x= 50;
+{
+
+    console.log(x);
+}
+
+
+
 // templet string 
 
-console.log(a,b);
+// console.log(a,b);
 // the value of a is a and the value of b is b
-console.log("the value of a is"+a+"and the value of b is"+b);
+// console.log("the value of a is"+a+"and the value of b is"+b);
 var a =50 
  b= 70; // This is an error
-console.log(`the value of a is ${a} and the value of b is ${b}`);
+// console.log(`the value of a is ${a} and the value of b is ${b}`);
 // c = 95;
